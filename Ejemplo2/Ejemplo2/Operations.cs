615245b5e700c8e13042a4e8bc0cb0985acbff56
rene ulises <rene_ulises@me.com>
2018-05-11 18:13:51 -0500

    Ejemplo suma resta con parametros en el constructor

---
+++ Ejemplo2/Ejemplo2/Operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Ejemplo2
+{
+    public class Operations
+    {
+		private int val1;
+        private int val2;
+        public Operations(int val1, int val2)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+        public int Adition()
+        {
+            return val1 + val2;
+        }
+
+        public int Substraction()
+        {
+            return val1 - val2;
+        }
+    }
+}
---
+++ Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejemplo2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			string val1, val2;
+            Console.WriteLine("Enter two numbers");
+            Console.WriteLine("Enter the first number");
+            val1 = Console.ReadLine();
+            Console.WriteLine("Enter the second number");
+            val2 = Console.ReadLine();
+            Operations myOperations = new Operations(Int32.Parse(val1), Int32.Parse(val2));
+            Console.WriteLine("La suma es:" + myOperations.Adition());
+            Console.WriteLine("La resta es:" + myOperations.Substraction());
+        }
+    }
+}

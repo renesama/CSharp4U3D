2f3a05c995cec78a0845deba4a4899856ed7b2e9
rene ulises <rene_ulises@me.com>
2018-05-11 18:06:55 -0500

    Ejemplo saludo usando una clase

---
+++ Ejemplo1/Ejemplo1/HelloFromClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Ejemplo1
+{
+    public class HelloFromClass
+    {
+        public HelloFromClass()
+        {
+			
+        }
+		public String Greeting(String text)
+        {
+            return " Hi there " + text;
+        }
+    }
+}
---
+++ Ejemplo1/Ejemplo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			String text;
+            HelloFromClass saludo = new HelloFromClass();
+            Console.WriteLine("Hello give me your name");
+            text = Console.ReadLine();
+            Console.WriteLine(saludo.Greeting(text));
+        }
+    }
+}

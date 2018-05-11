87d09447a6454853db0906b2a4e40185206d413a
rene ulises <rene_ulises@me.com>
2018-05-11 18:18:58 -0500

    Ejemplo suma con getter setter y parametros al metodo

---
+++ Ejemplo3/Ejemplo3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejemplo3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Suma miSuma = new Suma();
+            Console.WriteLine("Ingresa 2 numeros");
+            Console.WriteLine("Ingrea el valor 1");
+            miSuma.setVal1(Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("Ingrea el valor 2");
+            miSuma.setVal2(Int32.Parse(Console.ReadLine()));
+            Console.WriteLine("La suma de " + miSuma.getVal1() + " mas " + miSuma.getVal2() + " es:  " + miSuma.suma());
+        }
+    }
+}
---
+++ Ejemplo3/Ejemplo3/Suma.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Ejemplo3
+{
+	public class Suma{
+        int val1;
+        int val2;
+        public Suma()
+        {
+
+        }
+        public Suma(int i)
+        {
+
+        }
+
+        public Suma(int a, int b)
+        {
+
+        }
+        public int getVal1()
+        {
+            return val1;
+        }
+        public int getVal2()
+        {
+            return val2;
+        }
+        public void setVal1(int val1)
+        {
+            this.val1 = val1;
+        }
+        public void setVal2(int val2)
+        {
+            this.val2 = val2;
+        }
+        public int suma(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+        public int suma()
+        {
+            return val1 + val2;
+        }
+        public int resta()
+        {
+            return val1 - val2;
+        }
+
+    }
+}
